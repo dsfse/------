2bb8ab1c7ae13dd3b40842aba9f8b3e23b7c3bb5
sangmin <sangmin1465@gmail.com>
2022-10-27 17:59:58 +0900

    s

--- bntailor/js/main.js
+++ bntailor/js/main.js
@@ -11,9 +11,51 @@ $(document).ready(function(){
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.btn_pagin', /* 해당 요소의 class명 */
+            el: '.btn_paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
 
+    }); // visual swiper
+
+    /* 
+       visual popup의 정지버튼/재생버튼
+       하나의 버튼이 두가지 기능 - 정지와 재생 기능을 구분하는 값.... 
+       btn_stop 버튼에 play클래스가 없으면 일시정지
+       btn_stop 버튼에 play클래스가 있으면 재생 
+    */
+    $('.visual .popup .btn_wrap .btn_stop').on('click', function(){
+        let popStatus = $(this).hasClass('play'); //true면 play가 있다는 이야기 - 재생기능실행
+        if(popStatus == true){ // 버튼의 상태가 play 모양 - 현재 정지상태 - 재생 기능을 실행
+            swiper.autoplay.start();
+            $(this).removeClass('play');
+            $(this).text('일시정지');
+        }else{ // 버튼의 상태가 일시정지 모양 - 현재 재생상태 -일시정지 기능을 실행
+            swiper.autoplay.stop(); 
+            $(this).addClass('play');
+            $(this).text('재생');
+        }
+    }); // visual stop
+    
+    /* fabric 이미지 스크롤 효과 */
+    let scrolling;
+    let moveTop;
+    let objName = $('.fabric .bg img');
+    fabcScroll(); //로딩됐을때 한번
+    $(window).scroll(function(){ //스크롤 할때마다 실행
+        fabcScroll();
     });
-});
+
+    function fabcScroll(){ //스크롤 값을 계산해서, fabric의 이미지를 움직일 함수
+        /* 스크롤값을 요소의 위치값으로 변경해서 스타일을 적용 효과를 줄 요소가 화면의 하단
+        에 등장하기 시작했을 때부터 스크를 한 값을 새로 계산해서 해당 요소에 줘야 해당 요
+        소가 화면 어디에 배치되어 있든 자연스럽게 패럴럭스 효과를 줄 수 있음 */
+
+        scrolling =  $(window).scrollTop();
+        console.log(scrolling, 'scroll');
+        console.log(objName.offset().top, 'top');
+        moveTop = scrolling*0.15;
+        // objName.css('transform', 'translate(0, -'+moveTop+'px)');
+    }
+
+
+}); // document.ready
