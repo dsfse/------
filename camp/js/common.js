f2a914f5a6c55d26f6653aa27145c0990363b149
sangmin <sangmin1465@gmail.com>
2022-11-17 15:20:34 +0900

    s

--- camp/js/common.js
+++ camp/js/common.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
     $('.header .gnb .gnb_wrap > ul').on('mouseleave',function(){
         $('.header').removeClass('open')
     });
+
+    $('.footer .btn_top').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500);
+    });
+
 });
